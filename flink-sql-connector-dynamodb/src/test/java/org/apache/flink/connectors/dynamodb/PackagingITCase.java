ec8425943ddfccf39226b0f35150c326e23f263d
darenwkt <darenwkt@amazon.co.uk>
2022-10-12 08:07:29 +0100

    retry githuub action

---
+++ flink-sql-connector-dynamodb/src/test/java/org/apache/flink/connectors/dynamodb/PackagingITCase.java
@@ -0,0 +1,19 @@
+package org.apache.flink.connectors.dynamodb;
+
+import org.apache.flink.packaging.PackagingTestUtils;
+import org.apache.flink.test.resources.ResourceTestUtils;
+
+import org.junit.jupiter.api.Test;
+
+import java.nio.file.Path;
+import java.util.Arrays;
+
+/** Test to verify contents of packaged jar. */
+public class PackagingITCase {
+    @Test
+    void testPackaging() throws Exception {
+        final Path jar = ResourceTestUtils.getResource(".*/flink-sql-connector-dynamodb-[^/]*\\.jar");
+
+        PackagingTestUtils.assertJarContainsOnlyFilesMatching(jar, Arrays.asList("META-INF/"));
+    }
+}
